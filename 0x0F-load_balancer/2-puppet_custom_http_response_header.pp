d36033befebd8acdf683d56ff8265de9184d22c9
Modeste Ciira <ciiramodeste23@gmail.com>
2023-09-05 23:16:38 +0300

    changes made

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,13 +1,22 @@
 # configure nginx by creating a custom HTTP header response
-class { 'nginx':
-  ensure => 'installed',
+exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => 'shell',
+}
+exec { 'upgrade':
+  command  => 'sudo apt-get -y upgrade',
+  provider => 'shell',
+}
+exec { 'install_nginx':
+  provider => 'shell',
+  command  => '/usr/bin/apt-get -y install nginx',
 }
 file { '/etc/nginx/sites-available/default':
   ensure => present,
   notify => Exec['add_custom_header'],
 }
 exec { 'add_custom_header':
-  command     => "sed -i '54i\\tadd_header X-Served-By \$hostname;' /etc/nginx/sites-available/default",
+  command     => "sed -i '54i\\tadd_header X-Served-By $HOSTNAME;' /etc/nginx/sites-available/default",
   path        => '/bin:/usr/bin',
   refreshonly => true,
   subscribe   => File['/etc/nginx/sites-available/default'],
