b90864935e9493ef9077494138b4c949bf83e624
Modeste Ciira <ciiramodeste23@gmail.com>
2023-09-06 00:39:36 +0300

    error fixed

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,9 @@
 # configure nginx
-package { 'nginx':
-  ensure => 'installed',
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx',
+  provider => shell,
 }
-
 file { '/etc/nginx/sites-available/default':
   content => template('path/to/nginx_config.erb'),
   notify  => Service['nginx'],
@@ -16,4 +17,8 @@ service { 'nginx':
 exec { 'add_custom_header':
   command => "sed -i '/listen 80 default_server;/a add_header X-Served-By ${hostname};' /etc/nginx/sites-available/default",
   require => File['/etc/nginx/sites-available/default'],
+}
+exec { 'Nginx restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
