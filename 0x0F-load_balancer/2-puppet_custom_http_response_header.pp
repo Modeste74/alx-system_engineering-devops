4c1e97cde90d91a8cf7e01681c7e656a7932a97f
Modeste Ciira <ciiramodeste23@gmail.com>
2023-09-05 23:24:51 +0300

    testing something

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,29 +1,8 @@
-# configure nginx by creating a custom HTTP header response
-exec { 'update':
-  command  => 'sudo apt-get update',
+# Installs Nginx and custom HTTP response headers
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
   provider => shell,
-}
-exec { 'upgrade':
-  command  => 'sudo apt-get -y upgrade',
-  provider => shell,
-}
-exec { 'install_nginx':
-  provider => shell,
-  command  => '/usr/bin/apt-get -y install nginx',
-}
-file { '/etc/nginx/sites-available/default':
-  ensure => present,
-  notify => Exec['add_custom_header'],
-}
-exec { 'add_custom_header':
-  command     => "sed -i '54i\\tadd_header X-Served-By $HOSTNAME;' /etc/nginx/sites-available/default",
-  path        => '/bin:/usr/bin',
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/sites-available/default'],
-}
-exec { 'restart_nginx':
-  command     => 'sudo service nginx restart',
-  provider    => shell,
-  refreshonly => true,
-  subscribe   => Exec['add_custom_header'],
 }
