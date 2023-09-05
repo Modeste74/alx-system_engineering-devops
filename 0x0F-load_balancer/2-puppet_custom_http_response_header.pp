bf96ae848eff02669c2728f777f179388bf00be9
Modeste Ciira <ciiramodeste23@gmail.com>
2023-09-05 23:20:28 +0300

    error dealt with

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,14 +1,14 @@
 # configure nginx by creating a custom HTTP header response
 exec { 'update':
   command  => 'sudo apt-get update',
-  provider => 'shell',
+  provider => shell,
 }
 exec { 'upgrade':
   command  => 'sudo apt-get -y upgrade',
-  provider => 'shell',
+  provider => shell,
 }
 exec { 'install_nginx':
-  provider => 'shell',
+  provider => shell,
   command  => '/usr/bin/apt-get -y install nginx',
 }
 file { '/etc/nginx/sites-available/default':
@@ -22,8 +22,8 @@ exec { 'add_custom_header':
   subscribe   => File['/etc/nginx/sites-available/default'],
 }
 exec { 'restart_nginx':
-  command     => 'service nginx restart',
-  provider    => 'shell',
+  command     => 'sudo service nginx restart',
+  provider    => shell,
   refreshonly => true,
   subscribe   => Exec['add_custom_header'],
 }
