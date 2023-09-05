72c213c62f74dff7285c4d74a2b0c98b5cf1432d
Modeste Ciira <ciiramodeste23@gmail.com>
2023-09-06 00:45:04 +0300

    error tackled

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,8 @@
-# configure nginx
+# Installs Nginx and custom HTTP response headers
 exec { 'command':
   command  => 'apt-get -y update;
-  apt-get -y install nginx',
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
   provider => shell,
-}
-file { '/etc/nginx/sites-available/default':
-  content => template('path/to/nginx_config.erb'),
-  notify  => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
-}
-
-exec { 'add_custom_header':
-  command => "sed -i '/listen 80 default_server;/a add_header X-Served-By ${hostname};' /etc/nginx/sites-available/default",
-  require => File['/etc/nginx/sites-available/default'],
-}
-exec { 'Nginx restart':
-  provider => shell,
-  command  => 'sudo service nginx restart',
 }
